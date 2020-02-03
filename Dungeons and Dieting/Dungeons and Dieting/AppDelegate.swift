e80f8fda15976101c914347695b082b6baf9d138
Arifa tyrell <arifa.tyrell@gmail.com>
2020-02-03 15:02:07 -0500

    Added fire base
    added fire base to the project now we can see user data

--- Dungeons and Dieting/Dungeons and Dieting/AppDelegate.swift
+++ Dungeons and Dieting/Dungeons and Dieting/AppDelegate.swift
@@ -1,13 +1,5 @@
-//
-//  AppDelegate.swift
-//  Dungeons and Dieting
-//
-//  Created by Matthew Ferreira on 2020-01-21.
-//  Copyright © 2020 ThreePlayerProductions. All rights reserved.
-//
-
 import UIKit
-
+import FirebaseCore
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -16,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
@@ -40,7 +33,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
+    
 }
 

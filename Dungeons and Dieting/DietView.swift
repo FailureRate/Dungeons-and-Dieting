44d08ba6aab9c5849b2594e42c5d87ae48f0cfd7
Raphael Rodriguez <32069809+FailureRate@users.noreply.github.com>
2020-02-18 16:24:23 -0500

    DietVeiw Cont
    Working on textViews

--- Dungeons and Dieting/DietView.swift
+++ Dungeons and Dieting/DietView.swift
@@ -9,9 +9,25 @@
 import Foundation
 import UIKit
 class DietView:UIViewController{
-    var textField:UITextField;
+    var calorieTextField0:UITextField;
+    var calorieTextField1:UITextField;
+    
+    init(){
+        calorieTextField0 = UITextField();
+        calorieTextField1 = UITextField();
+        calorieTextField0.text = "Please Input Calories"
+        calorieTextField1.text = "Please Input Calories"
+        super.init(nibName:nil, bundle: nil)
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(calorieTextField0);
+        view.addSubview(calorieTextField1);
+    }
+    
+    
 }

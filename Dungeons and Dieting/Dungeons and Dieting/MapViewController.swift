a4cd7d567b280e20b5f452c21a07866d42d57d14
Arifa tyrell <arifa.tyrell@gmail.com>
2020-02-19 00:12:40 -0500

    trying to fix error
    undoing mistake reverting back to different point

--- Dungeons and Dieting/Dungeons and Dieting/MapViewController.swift
+++ Dungeons and Dieting/Dungeons and Dieting/MapViewController.swift
@@ -19,30 +19,25 @@ class MapViewController: UIViewController {
     var shopButton: UIButton
     
     init() {
-        friendButton = UIButton()
-        itemButton = UIButton()
-        mapButton = UIButton()
-        progressButton = UIButton()
-        shopButton = UIButton()
+        friendButton = UIButton(frame: CGRect (x: 0, y: 0, width: 50, height: 50))
+        itemButton = UIButton(frame: CGRect (x: 0, y: 0, width: 50, height: 50))
+        mapButton = UIButton(frame: CGRect (x: 0, y: 0, width: 50, height: 50))
+        progressButton = UIButton(frame: CGRect (x: 0, y: 0, width: 50, height: 50))
+        shopButton = UIButton(frame: CGRect (x: 0, y: 0, width: 50, height: 50))
         super.init(nibName:nil, bundle: nil)
     }
 
     required init?(coder aDecoder: NSCoder) {
-        friendButton = UIButton()
-        itemButton = UIButton()
-        mapButton = UIButton()
-        progressButton = UIButton()
-        shopButton = UIButton()
+        friendButton = UIButton(frame: CGRect (x: 0, y: 0, width: 50, height: 50))
+        itemButton = UIButton(frame: CGRect (x: 0, y: 0, width: 50, height: 50))
+        mapButton = UIButton(frame: CGRect (x: 0, y: 0, width: 50, height: 50))
+        progressButton = UIButton(frame: CGRect (x: 0, y: 0, width: 50, height: 50))
+        shopButton = UIButton(frame: CGRect (x: 0, y: 0, width: 50, height: 50))
         super.init(coder: aDecoder)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        friendButton = UIButton(frame: CGRect (x: (view.bounds.maxX - 50) / 2 - 200, y: view.bounds.maxY - 150, width: 50, height: 50))
-        itemButton = UIButton(frame: CGRect (x: (view.bounds.maxX - 50) / 2 - 100, y: view.bounds.maxY - 150, width: 50, height: 50))
-        mapButton = UIButton(frame: CGRect (x: (view.bounds.maxX - 50) / 2, y: view.bounds.maxY - 150, width: 50, height: 50))
-        progressButton = UIButton(frame: CGRect (x: (view.bounds.maxX - 50) / 2 + 100, y: view.bounds.maxY - 150, width: 50, height: 50))
-        shopButton = UIButton(frame: CGRect (x: (view.bounds.maxX - 50) / 2 + 200, y: view.bounds.maxY - 150, width: 50, height: 50))
         
         view.addSubview(friendButton)
         view.addSubview(itemButton)
@@ -50,46 +45,29 @@ class MapViewController: UIViewController {
         view.addSubview(progressButton)
         view.addSubview(shopButton)
         
-        friendButton.setImage(UIImage(named: "Button"), for: .normal)
-        itemButton.setImage(UIImage(named: "Button"), for: .normal)
-        mapButton.setImage(UIImage(named: "Button"), for: .normal)
-        progressButton.setImage(UIImage(named: "Button"), for: .normal)
-        shopButton.setImage(UIImage(named: "Button"), for: .normal)
+        friendButton.setImage(UIImage(named: "circle"), for: .normal)
+        friendButton.translatesAutoresizingMaskIntoConstraints = false
+        friendButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        friendButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
-        friendButton.addTarget(self, action: #selector(toMap), for: .touchUpInside)
-        itemButton.addTarget(self, action: #selector(toMap), for: .touchUpInside)
-        mapButton.addTarget(self, action: #selector(toMap), for: .touchUpInside)
-        progressButton.addTarget(self, action: #selector(toMap), for: .touchUpInside)
-        shopButton.addTarget(self, action: #selector(toMap), for: .touchUpInside)
-    }
-    
-    @IBAction func toFriend() {
-        let newViewController = FriendViewController()
-        newViewController.view.backgroundColor = .green
-        self.present(newViewController, animated: false, completion: nil)
-    }
-    
-    @IBAction func toItem() {
-        let newViewController = InventoryViewController()
-        newViewController.view.backgroundColor = .green
-        self.present(newViewController, animated: false, completion: nil)
-    }
-    
-    @IBAction func toMap() {
-        let newViewController = MapViewController()
-        newViewController.view.backgroundColor = .green
-        self.present(newViewController, animated: false, completion: nil)
-    }
-    
-    @IBAction func toProgress() {
-        let newViewController = ProgressViewController()
-        newViewController.view.backgroundColor = .green
-        self.present(newViewController, animated: false, completion: nil)
-    }
-    
-    @IBAction func toShop() {
-        let newViewController = ShopViewController()
-        newViewController.view.backgroundColor = .green
-        self.present(newViewController, animated: false, completion: nil)
+        itemButton.setImage(UIImage(named: "circle"), for: .normal)
+        itemButton.translatesAutoresizingMaskIntoConstraints = false
+        itemButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        itemButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        mapButton.setImage(UIImage(named: "circle"), for: .normal)
+        mapButton.translatesAutoresizingMaskIntoConstraints = false
+        mapButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        mapButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        progressButton.setImage(UIImage(named: "circle"), for: .normal)
+        progressButton.translatesAutoresizingMaskIntoConstraints = false
+        progressButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        progressButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        shopButton.setImage(UIImage(named: "circle"), for: .normal)
+        shopButton.translatesAutoresizingMaskIntoConstraints = false
+        shopButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        shopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 }

edd23014843f635603cc2b00ef9122597552e50b
Arifa tyrell <arifa.tyrell@gmail.com>
2020-02-25 16:19:07 -0500

    Update InventoryViewController.swift
    text is now displayed

--- Dungeons and Dieting/Dungeons and Dieting/InventoryViewController.swift
+++ Dungeons and Dieting/Dungeons and Dieting/InventoryViewController.swift
@@ -20,9 +20,8 @@ class InventoryViewController:UIViewController
     var progressButton: UIButton
     var shopButton: UIButton
     
-  
     var HealthPtext:UILabel
-    @IBOutlet var LastWtext:UILabel!
+      var Leveltext:UILabel
     var CurWtext:UILabel
     var PlayNamtext:UILabel
 
@@ -30,10 +29,13 @@ class InventoryViewController:UIViewController
     let Cweight:Int
     let lastwe:Int
     
+    var LastWtext:UILabel!
+
+
     init() {
- 
+
         HealthPtext = UILabel()
-        LastWtext = UILabel()
+        Leveltext = UILabel()
         CurWtext = UILabel()
         PlayNamtext = UILabel()
         level = 0
@@ -60,7 +62,33 @@ class InventoryViewController:UIViewController
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        let framelable :CGRect = CGRect(x:10,y:50,width: UIScreen.main.bounds.width - 40 , height: 100)
+          let framelable2 :CGRect = CGRect(x:10,y:10,width: UIScreen.main.bounds.width - 45 , height: 150)
+          let framelable3 :CGRect = CGRect(x:50,y:20,width: UIScreen.main.bounds.width - 60 , height: 190)
+          let framelable4 :CGRect = CGRect(x:20,y:40,width: UIScreen.main.bounds.width - 90 , height: 200)
+          let framelable5 :CGRect = CGRect(x:70,y:40,width: UIScreen.main.bounds.width - 80 , height: 200)
+        
+        LastWtext =  UILabel(frame: framelable)
+        LastWtext.text = "last Cal"
+        
+        CurWtext  =  UILabel(frame: framelable2)
+        CurWtext.text = " cur cal"
+       
+        PlayNamtext =  UILabel(frame: framelable3)
+        PlayNamtext.text = "name"
+        
+       HealthPtext =  UILabel(frame: framelable4)
+        HealthPtext.text  = "health"
+        
+        Leveltext  =  UILabel(frame: framelable5)
+        Leveltext.text = "Level"
+        
+        self.view.addSubview(LastWtext)
+        self.view.addSubview(CurWtext)
+        self.view.addSubview(PlayNamtext)
+        self.view.addSubview(HealthPtext)
+        self.view.addSubview(Leveltext)
+        
         friendButton = UIButton(frame: CGRect (x: (view.bounds.maxX - 50) / 2 - 200, y: view.bounds.maxY - 150, width: 50, height: 50))
         itemButton = UIButton(frame: CGRect (x: (view.bounds.maxX - 50) / 2 - 100, y: view.bounds.maxY - 150, width: 50, height: 50))
         mapButton = UIButton(frame: CGRect (x: (view.bounds.maxX - 50) / 2, y: view.bounds.maxY - 150, width: 50, height: 50))
@@ -112,7 +140,16 @@ class InventoryViewController:UIViewController
         newViewController.modalPresentationStyle = .fullScreen
         self.present(newViewController, animated: false, completion: nil)
     }
+
     
+  
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+  
 }
 
 

785a4c8a6d86cae35b6b4187aa9ece93273aec08
Arifa tyrell <arifa.tyrell@gmail.com>
2020-04-07 03:34:07 -0400

    update Monsters
    Monster will now appear in random locations on the map

--- Dungeons and Dieting/Dungeons and Dieting/MapViewController.swift
+++ Dungeons and Dieting/Dungeons and Dieting/MapViewController.swift
@@ -30,12 +30,36 @@ class MapViewController: UIViewController , CLLocationManagerDelegate, MKMapView
     let LocationM = CLLocationManager()
     let Clientid = "WP0KU1PZ5EZELDOTL2MVVWEK0Y4PSU45TMHLHUCV4QFBK4OI"
     let CLIENTSECRET = "FJJPDA5ANL3JCXAUALOMXKLWJYKSUY1PTZXESPGYO04RA1RK"
-     
+    
+    var randomLoc: Double
+    var randomLoc2: Double
+    var randomLoc3: Double
+     var randomLoc4: Double
+    var randomLoc5: Double
+     var randomLoc6: Double
+    var randomLoc7: Double
+    var randomLoc8: Double
+    
+     var Wolfpin1 = MKPointAnnotation()
+     var Flypin2 = MKPointAnnotation()
+     var Batpin3 = MKPointAnnotation()
+     var SlimeG4 = MKPointAnnotation()
+     var SlimePpin5 = MKPointAnnotation()
+     var SlimeRpin6 = MKPointAnnotation()
+     var SlimeYpin7 = MKPointAnnotation()
+     var Dragonpin8 = MKPointAnnotation()
 
- var randomLoc = Double.random(in:-100...1000)
+    
     init() {
- 
-     
+        randomLoc =  0.004065
+        randomLoc2 =  Double.random(in:0...00.5)
+        randomLoc3 =  Double.random(in:0...00.5)
+        randomLoc4 =  Double.random(in:0...00.5)
+        randomLoc5 =  Double.random(in:0...00.5)
+        randomLoc6 =  Double.random(in:0...00.5)
+        randomLoc7 =  Double.random(in:0...00.5)
+        randomLoc8 =  Double.random(in:0...00.5)
+        
         mapview = MKMapView()
         friendButton = UIButton()
         itemButton = UIButton()
@@ -53,6 +77,15 @@ class MapViewController: UIViewController , CLLocationManagerDelegate, MKMapView
         mapButton = UIButton()
         progressButton = UIButton()
         shopButton = UIButton()
+        
+        randomLoc =   0.004065
+        randomLoc2 =   Double.random(in:0...00.5)
+        randomLoc3 =  Double.random(in:0...00.5)
+        randomLoc4 = Double.random(in:0...00.5)
+        randomLoc5 =  Double.random(in:0...00.5)
+        randomLoc6 =  Double.random(in:0...00.5)
+        randomLoc7 =  Double.random(in:0...00.5)
+        randomLoc8 =  Double.random(in:0...00.5)
         super.init(coder: aDecoder)
     }
     
@@ -80,15 +113,23 @@ class MapViewController: UIViewController , CLLocationManagerDelegate, MKMapView
         itemButton.addTarget(self, action: #selector(toItem), for: .touchUpInside)
         progressButton.addTarget(self, action: #selector(toProgress), for: .touchUpInside)
         shopButton.addTarget(self, action: #selector(toShop), for: .touchUpInside)
-                  setupLoation()
         
         
 
-             checkLocationServices()
-             self.mapview = MKMapView(frame: CGRect(x: 10, y: 60, width: 400, height: 600))
-
-              self.view.addSubview(mapview)
-          
+        
+        checkLocationServices()
+        self.mapview = MKMapView(frame: CGRect(x: 10, y: 60, width: 400, height: 600))
+        
+        self.randomLoc = 0.004065
+        self.randomLoc2 = Double.random(in:0...0.004065)
+           self.randomLoc3 =  Double.random(in:0...0.004065)
+            self.randomLoc4 =  Double.random(in:0...0.004065)
+           self.randomLoc5 =  Double.random(in:0...0.004065)
+            self.randomLoc6 =  Double.random(in:0...0.004065)
+            self.randomLoc7 =  Double.random(in:0...0.004065)
+             self.randomLoc8 =  Double.random(in:0...0.004065)
+ 
+        self.view.addSubview(mapview)
     }
     
     @IBAction func toFriend() {
@@ -119,21 +160,20 @@ class MapViewController: UIViewController , CLLocationManagerDelegate, MKMapView
         self.present(newViewController, animated: false, completion: nil)
     }
     
+    
+    //check to see location system are working
     func checkLocationServices()
      {
          if(CLLocationManager.locationServicesEnabled())
          {
              setupLoation()
              checkLocationAuthorization()
+      
          }
      }
    
-    
-    ///marker info
 
-        
-
-    
+    // sets up Loation
     func setupLoation()
           {
               LocationM.delegate = self
@@ -141,17 +181,29 @@ class MapViewController: UIViewController , CLLocationManagerDelegate, MKMapView
               LocationM.startUpdatingLocation()
           }
     
+    //set map foucus on this region
     func centerviewuserlocation(){
         if let location = LocationM.location?.coordinate{
             let region = MKCoordinateRegion.init(center: location, latitudinalMeters: 1000, longitudinalMeters: 1000)
             mapview.setRegion(region, animated: true)
+            
+   
+       //adds Annotation to map
+          mapview.addAnnotation(Wolfpin1)
+          mapview.addAnnotation(Flypin2)
+          mapview.addAnnotation(Batpin3 )
+          mapview.addAnnotation(SlimeG4)
+          mapview.addAnnotation(SlimePpin5)
+          mapview.addAnnotation(SlimeRpin6)
+          mapview.addAnnotation(SlimeYpin7)
+          mapview.addAnnotation(Dragonpin8)
             
         }
         
     }
 
     
-    
+    //check user location Authorization
     func checkLocationAuthorization() {
         switch CLLocationManager.authorizationStatus() {
         case .authorizedWhenInUse:
@@ -176,18 +228,64 @@ class MapViewController: UIViewController , CLLocationManagerDelegate, MKMapView
         
     }
 
-       
+       //update location of user
        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
        {
            if let location =  locations.last
            {
                if location.horizontalAccuracy > 0 {
                    LocationM.stopUpdatingLocation()
-                   let longitude =  location.coordinate.longitude
-                   let latitude  = location.coordinate.latitude
-                
+              
+                let longitude =  location.coordinate.longitude
+                let latitude  = location.coordinate.latitude
                 let centerview = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
                 let region = MKCoordinateRegion.init(center: centerview, latitudinalMeters: 1000, longitudinalMeters: 1000)
+                
+                
+                //monster location Coord
+                let monster1Loc = CLLocationCoordinate2DMake(latitude + randomLoc, longitude + randomLoc2 )
+                
+                let monster1Loc2 = CLLocationCoordinate2DMake(latitude + randomLoc2 ,longitude + randomLoc3 )
+             
+                let monster1Loc3 = CLLocationCoordinate2DMake(latitude + randomLoc3 , longitude + randomLoc4 )
+                
+                let monster1Loc4 = CLLocationCoordinate2DMake(latitude + randomLoc4  ,longitude + randomLoc5)
+                
+                let monster1Loc5 = CLLocationCoordinate2DMake(latitude + randomLoc5 ,longitude + randomLoc6)
+                 let monster1Loc6 = CLLocationCoordinate2DMake(latitude + randomLoc6 ,longitude + randomLoc7)
+                
+                 let monster1Loc7 = CLLocationCoordinate2DMake(latitude + randomLoc7 ,longitude + randomLoc7)
+                
+                //monster location Coord
+                Wolfpin1.coordinate = monster1Loc
+                Wolfpin1.title = "wolf"
+                
+                Flypin2.coordinate = monster1Loc2
+                Flypin2.title = "fly"
+                
+                Batpin3.coordinate = monster1Loc3
+                Batpin3.title = "bat"
+                
+                SlimeG4.coordinate = monster1Loc4
+                SlimeG4.title = "slime green"
+                
+                SlimePpin5.coordinate = monster1Loc5
+                SlimePpin5.title = "slime purple"
+                
+                SlimeRpin6.coordinate = monster1Loc6
+                SlimeRpin6.title = "slime red"
+                
+                SlimeYpin7.coordinate = monster1Loc7
+                SlimeYpin7.title = "slime yellow"
+                
+
+                print("monster \(String(describing: Wolfpin1.coordinate))")
+                print("monplayer \(String(describing: LocationM.location?.coordinate))")
+                print("monster1 \(String(describing: Batpin3.coordinate))")
+                 print("monster2 \(String(describing: Flypin2.coordinate))")
+                 print("monster3 \(String(describing: Wolfpin1.coordinate))")
+             
+                
                 mapview.setRegion(region, animated: true)
                }
            }
@@ -214,17 +312,44 @@ class MapViewController: UIViewController , CLLocationManagerDelegate, MKMapView
            
        }
 
-       
+    
+    
+    
+  
+       ///checks to see if location falied
        func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
            print("Location update falied , \(error)")
        }
        
     
-       
+        ///checks to see if location Authorization has been changed
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus)
     {
      checkLocationAuthorization()
     }
 
+    
+    
+      ///checks to see MKPointAnnotation identifed
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?  {
+         
+         if !(annotation is MKPointAnnotation)
+         {
+             print ("nit reg as mark")
+             return nil
+         }
+         
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "monsteridentifer")
+         if annotationView == nil {
+             annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "monsteridentifer")
+             annotationView!.canShowCallout = true
+         }
+         else{
+             annotationView!.annotation = annotation
+         }
+         annotationView!.image = UIImage(named: "Wolf2.png")
+         
+         return annotationView
+     }
 
 }

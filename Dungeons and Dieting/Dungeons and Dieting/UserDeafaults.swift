0c9201ead7cef1ae1e2e4a5f330b1815321ebc29
Raphael Rodriguez <32069809+FailureRate@users.noreply.github.com>
2020-04-07 02:20:23 -0400

    Saved Calorie Data with Userdefaults

--- Dungeons and Dieting/Dungeons and Dieting/UserDeafaults.swift
+++ Dungeons and Dieting/Dungeons and Dieting/UserDeafaults.swift
@@ -7,3 +7,56 @@
 //
 
 import Foundation
+
+extension UserDefaults{
+    func setValueForCalories(value: String?){
+        if value != nil {
+            UserDefaults.standard.set(value, forKey: "Calorie Amount")
+        }else{
+            UserDefaults.standard.removeObject(forKey: "Calorie Amount")
+        }
+        UserDefaults.standard.synchronize()
+    }
+    
+    func setValueForCalories1(value: String?){
+        if value != nil {
+            UserDefaults.standard.set(value, forKey: "Calorie Amount1")
+        }else{
+            UserDefaults.standard.removeObject(forKey: "Calorie Amount1")
+        }
+        UserDefaults.standard.synchronize()
+    }
+
+    func setValueForCalories2(value: String?){
+        if value != nil {
+            UserDefaults.standard.set(value, forKey: "Calorie Amount2")
+        }else{
+            UserDefaults.standard.removeObject(forKey: "Calorie Amount2")
+        }
+        UserDefaults.standard.synchronize()
+    }
+
+    func setValueForCalories3(value: String?){
+        if value != nil {
+            UserDefaults.standard.set(value, forKey: "Calorie Amount3")
+        }else{
+            UserDefaults.standard.removeObject(forKey: "Calorie Amount3")
+        }
+        UserDefaults.standard.synchronize()
+    }
+
+    func getValueofCalories() -> String?{
+        return UserDefaults.standard.value(forKey: "Calorie Amount") as? String
+    }
+    
+      func getValueofCalories1() -> String?{
+          return UserDefaults.standard.value(forKey: "Calorie Amount1") as? String
+      }
+    
+      func getValueofCalories2() -> String?{
+          return UserDefaults.standard.value(forKey: "Calorie Amount2") as? String
+      }
+    
+      func getValueofCalories3() -> String?{
+          return UserDefaults.standard.value(forKey: "Calorie Amount3") as? String
+      }}
